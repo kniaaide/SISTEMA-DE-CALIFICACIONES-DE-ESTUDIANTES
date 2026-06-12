9c12e88b99635e920d21c88086d9c32e2ce55626
keniaaide <vallesteroskeniaaide@gmail.com>
2026-06-12 15:42:52 -0600

    Agregar archivos de proyecto.

---
+++ SISTEMA DE CALIFICACIONES DE ESTUDIANTES/Form1.cs
@@ -0,0 +1,10 @@
+namespace SISTEMA_DE_CALIFICACIONES_DE_ESTUDIANTES
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
